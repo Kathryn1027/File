5fb46ad6a86a27ebf1c500c811967897451b8310
Kathryn1027 <68544025+Kathryn1027@users.noreply.github.com>
2020-08-08 23:43:32 +0800

    Add files via upload

--- File.py
+++ File.py
@@ -1,9 +1,8 @@
 import os
 dirPath = r"C:\\Users\\tjen\\Desktop\\Python"
 result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
-#filelist=[]
-with open('list.csv', 'w') as f:
+with open('list.csv', 'w', encoding ='UTF-8') as f:
+	f.write('File Name\n')
 	for l in result:
 		if '.JPG' in l:
-			#filelist.append(l)
-			f.write(l + '\n')
+			f.write(str(l)+ '\n')
