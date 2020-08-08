86f6247874d7b62cc088e00ddb2fb3404e83d2bd
Kathryn1027 <68544025+Kathryn1027@users.noreply.github.com>
2020-08-09 01:09:42 +0800

    split the name and the file type

--- File.py
+++ File.py
@@ -2,7 +2,8 @@ import os
 dirPath = r"C:\\Users\\tjen\\Desktop\\Python"
 result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
 with open('list.csv', 'w', encoding ='UTF-8') as f:
-	f.write('File Name\n')
+	f.write('File Name, File Type\n')
 	for l in result:
 		if '.JPG' in l:
-			f.write(str(l)+ '\n')
+			n,b =str(l).split('.')
+			f.write(n +','+ b + '\n')
